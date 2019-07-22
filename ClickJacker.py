b55767c6af384c72e9dedda05e0aea901eb64842
Tibinsunny <33937416+Tibinsunny@users.noreply.github.com>
2019-07-22 22:18:23 +0530

    Add files via upload

---
+++ ClickJacker.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import urllib2
+#Coded and Developed By Tibin Sunny
+print("""\
+
+ _______________________________________________________________
+	   _____ _ _      _        _            _             
+	 / ____| (_)    | |      | |          | |            
+	| |    | |_  ___| | __   | | __ _  ___| | _____ _ __ 
+	| |    | | |/ __| |/ /   | |/ _` |/ __| |/ / _ \ '__|
+	| |____| | | (__|   < |__| | (_| | (__|   <  __/ |   
+	 \_____|_|_|\___|_|\_\____/ \__,_|\___|_|\_\___|_|  
+
+                                                     (Version 1.0)  
+
+        #Developed By TibinSunny #Contact:tibinsunny95@gmail.com
+		            Feel Free to Report Bugs
+ ________________________________________________________________  
+   """)
+hdr = {'User-Agent':'Mozilla/5.0'}
+opt=input("Enter 1:For Bulk url checking \nEnter 2:For Single url checking: ")
+if opt==1:
+	file=raw_input("Enter the path to text file (c:/asd/example.txt):  ")
+	fh = open(file)
+        for line in fh:
+        	#print(line)
+		if "http" not in line:
+                	tester_tibin="https://"+line
+                
+                try:
+                	req = urllib2.Request(tester_tibin,headers=hdr)
+                	response = urllib2.urlopen(req)
+	       		headers = response.info()
+	        	#print response.info()
+	        	if  "X-Frame-Options: DENY" in headers:
+				out_url=tester_tibin+"is Not Vulnerable"
+				print out_url
+				print " "
+	        	#elif "X-Frame-Options: SAMEORIGIN":
+		        	#out_url=tester_tibin+ " Is Vulnerable with Same Origin(manual test required)"
+				#print out_url
+				#print " "
+	        	else:
+		        	out_url=tester_tibin+" Is Vulnerable"
+				print out_url
+				print " "
+		except:
+  			print(tester_tibin +"   Cannot be reached")
+    			print " "
+elif opt==2:
+	url = raw_input("Enter the Domain name  :")
+	tester_tibin=url
+	if "http" not in url:
+		tester_tibin="http://"+url
+    	req = urllib2.Request(tester_tibin,headers=hdr)
+        response = urllib2.urlopen(req)
+	headers = response.info()
+	if  "X-Frame-Options: DENY" in headers:
+		out_url=tester_tibin+"is Not Vulnerable"
+		print out_url
+	#elif "X-Frame-Options: SAMEORIGIN":
+        	#out_url=tester_tibin+ " Is Vulnerable with Same Origin(manual test required)"
+		#print out_url
+		#print " "
+	
+	else:
+		out_url=tester_tibin+" Is Vulnerable"
+		print out_url
+		print " "
+
